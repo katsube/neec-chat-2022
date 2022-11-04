c6db045f16df454546628179c849b63160b37d6f
Makito Katsube <katsubemakito@gmail.com>
2022-11-05 00:27:32 +0900

    トークン対応

--- serve.js
+++ serve.js
@@ -1,17 +1,46 @@
+const crypto = require('crypto');
 const express = require('express');
 const app = express();
 const http = require('http').Server(app);
 const io = require('socket.io')(http);
 
-app.get("/", (req, res) => {
-  res.sendFile(__dirname + "/index.html");
+//-----------------------------------------------
+// ルーティング（express）
+//-----------------------------------------------
+app.get('/', (req, res) => {
+  res.sendFile(__dirname + '/index.html');
 });
 
+//-----------------------------------------------
+// Socket.IO
+//-----------------------------------------------
 io.on('connection', (socket) => {
-  socket.on("post", (data) => {
+  console.log('新しいユーザーが接続しました');
+
+  // 接続時にトークンを作成しクライアントに送信
+  const token = createToken();
+  io.to(socket.id).emit('token', token);    // 本人にだけ送信
+
+  // 発言を受信したら全クライアントに送信
+  socket.on('post', (data) => {
     console.log(data);
-    io.emit("member-post", data);
+    io.emit('member-post', data);
   });
 });
 
-http.listen(3000);
+// サーバを起動する
+http.listen(3000, () => {
+  console.log('サーバを起動しました。localhost:3000');
+});
+
+
+/**
+ * トークンを作成し返却
+ *
+ * @returns {string}
+ */
+function createToken() {
+  const uuid = crypto.randomUUID();              // 例:1b9d6bcd-bbfd-4b2d-9b5d-ab8dfbbd4bed
+  const token = 'TK' + uuid.replaceAll('-', ''); // 例:TK1b9d6bcdbbfd4b2d9b5dab8dfbbd4bed
+  return(token);
+}
